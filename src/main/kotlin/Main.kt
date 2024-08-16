f6c7061d330dab547cccc9e762cb3d275e147ef2
Abhishek Arya <aarya@atlassian.com>
2024-08-16 21:23:50 +0530

    add PING respons PONG command

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,28 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 import java.net.ServerSocket
 
 fun main(args: Array<String>) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-//    System.err.println("Logs from your program will appear here!")
 
-     var serverSocket = ServerSocket(6379)
+     val serverSocket = ServerSocket(6379)
 
-     // Since the tester restarts your program quite often, setting SO_REUSEADDR
-     // ensures that we don't run into 'Address already in use' errors
      serverSocket.reuseAddress = true
 
-     serverSocket.accept() // Wait for connection from client.
+     val clientSocket = serverSocket.accept()
+
+     val reader = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
+     val writer = OutputStreamWriter(clientSocket.getOutputStream())
+
+     val command = reader.readLine()
+     println("Received command: $command")
+
+     if (command == "PING") {
+          writer.write("+PONG\r\n")
+          writer.flush()
+     }
+
      println("accepted new connection")
+
+     clientSocket.close()
 }
