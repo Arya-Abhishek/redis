e70aff375ce984a5aadd3e5cecb821f994830119
Abhishek Arya <aarya@atlassian.com>
2024-09-09 22:54:56 +0530

    add support for ECHO command

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,28 +11,42 @@ fun main(args: Array<String>) {
      while (true) {
           val clientSocket = serverSocket.accept()
 
-          val reader = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
-          val writer = OutputStreamWriter(clientSocket.getOutputStream())
-
             Thread {
-                 try {
-                        var command = reader.readLine()
-                        do {
-                             if (command.trim().uppercase() == "PING") {
-                                writer.write("+PONG\r\n")
-                                writer.flush()
-                             }
-                             command = reader.readLine()
-                        } while (command != null)
-                 } catch (e: Exception) {
-                        println("Error: $e")
-                 } finally {
-                      try {
-                           clientSocket.close()
-                      } catch (e: java.lang.Exception) {
-                           println("Error closing socket: $e")
-                      }
-                 }
+                val reader = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
+                val writer = OutputStreamWriter(clientSocket.getOutputStream())
+
+                try {
+                    val command = reader.readText()
+                    val commandList = command.split("\r\n")
+                    val commandType = commandList[2]?.trim()?.uppercase()
+                    var resp = ""
+                    when (commandType) {
+                        "PING" -> {
+                            commandList.forEach {
+                                if (it.trim().uppercase() == "PING") {
+                                    resp += "+PONG\r\n"
+                                }
+                            }
+                        }
+                        "ECHO" -> {
+                            val idxOfEcho = commandList.indexOf("ECHO")
+                            for (i in idxOfEcho+1..<commandList.size) {
+                                if (commandList[i] in listOf("", "\n", "\r\n")) continue
+                                resp += "${commandList[i]}\r\n"
+                            }
+                        }
+                        else -> {
+                            resp = "-ERR unknown command '$commandType'\r\n"
+                        }
+                    }
+
+                    writer.write(resp)
+                    writer.flush()
+                } catch (e: Exception) {
+                    println("Error: $e")
+                } finally {
+                    clientSocket.close()
+                }
             }.start()
      }
 }
