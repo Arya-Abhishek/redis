aa71fda9aeec11a4085f029e4b89f179a649ba47
aarya <aarya@atlassian.com>
2024-11-03 14:26:33 +0530

    add support for reading multiple string values

--- src/test/java/MainKtTest.kt
+++ src/test/java/MainKtTest.kt
@@ -248,44 +248,71 @@ class MainKtTest{
         serverThread.interrupt()
     }
 
-//    @Test
-//    fun `test RDB file and GET command for single key value without any expiry`() {
-//        // Step 1: Create the RDB file
-//        val rdbFilePath = "/tmp/rdbfiles3974263201/orange.rdb"
-//        val rdbFile = File(rdbFilePath)
-//        rdbFile.parentFile.mkdirs()
-//        FileOutputStream(rdbFile).use { fos ->
-//            fos.write(byteArrayOf(
-//                0x52, 0x45, 0x44, 0x49, 0x53, 0x30, 0x30, 0x31, 0x31, 0xfa.toByte(), 0x09, 0x72, 0x65, 0x64, 0x69, 0x73,
-//                0x2d, 0x76, 0x65, 0x72, 0x05, 0x37, 0x2e, 0x32, 0x2e, 0x30, 0xfa.toByte(), 0x0a, 0x72, 0x65, 0x64, 0x69,
-//                0x73, 0x2d, 0x62, 0x69, 0x74, 0x73, 0xc0.toByte(), 0x40, 0xfe.toByte(), 0x00, 0xfb.toByte(), 0x01, 0x00, 0x00, 0x09, 0x70,
-//                0x69, 0x6e, 0x65, 0x61, 0x70, 0x70, 0x6c, 0x65, 0x0a, 0x73, 0x74, 0x72, 0x61, 0x77, 0x62, 0x65,
-//                0x72, 0x72, 0x79, 0xff.toByte(), 0xec.toByte(), 0xe1.toByte(), 0x7e, 0x8f.toByte(), 0xe0.toByte(), 0x32, 0x28, 0x22, 0x0a
-//            ))
-//        }
-//
-//        // Step 2: Run the Redis server
-//        val serverThread = Thread {
-//            main(arrayOf("--dir", "/tmp/rdbfiles3974263201", "--dbfilename", "orange.rdb"))
-//        }
-//        serverThread.start()
-//
-//        // Wait for the server to start
-//        Thread.sleep(1000)
-//
-//        // Step 3: Execute the GET command
-//        val socket = Socket("localhost", 6379)
-//        val writer = OutputStreamWriter(socket.getOutputStream())
-//        val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
-//
-//        writer.write("*2\r\n\$3\r\nGET\r\n\$9\r\npineapple\r\n")
-//        writer.flush()
-//
-//        val response = reader.readLine()
-//        assertEquals("\$10", response) // Length of "strawberry"
-//        assertEquals("strawberry", reader.readLine())
-//
-//        socket.close()
-//        serverThread.interrupt()
-//    }
+    @Test
+    fun `test RDB file and GET command for multiple key value without any expiry`() {
+        // Step 1: Create the RDB file
+        val rdbFilePath = "/tmp/rdbfiles3974263201/orange.rdb"
+        val rdbFile = File(rdbFilePath)
+        rdbFile.parentFile.mkdirs()
+
+        /*
+        * RDB file content -> having 5 key-value pairs
+        * JW4] Idx  | Hex                                             | ASCII
+[tester::#JW4] -----+-------------------------------------------------+-----------------
+[tester::#JW4] 0000 | 52 45 44 49 53 30 30 31 31 fa 09 72 65 64 69 73 | REDIS0011..redis
+[tester::#JW4] 0010 | 2d 76 65 72 05 37 2e 32 2e 30 fa 0a 72 65 64 69 | -ver.7.2.0..redi
+[tester::#JW4] 0020 | 73 2d 62 69 74 73 c0 40 fe 00 fb 05 00 00 09 72 | s-bits.@.......r
+[tester::#JW4] 0030 | 61 73 70 62 65 72 72 79 0a 73 74 72 61 77 62 65 | aspberry.strawbe
+[tester::#JW4] 0040 | 72 72 79 00 06 62 61 6e 61 6e 61 05 61 70 70 6c | rry..banana.appl
+[tester::#JW4] 0050 | 65 00 0a 73 74 72 61 77 62 65 72 72 79 06 6f 72 | e..strawberry.or
+[tester::#JW4] 0060 | 61 6e 67 65 00 05 61 70 70 6c 65 09 70 69 6e 65 | ange..apple.pine
+[tester::#JW4] 0070 | 61 70 70 6c 65 00 05 67 72 61 70 65 05 6d 61 6e | apple..grape.man
+[tester::#JW4] 0080 | 67 6f ff 04 3c 14 16 58 10 33 4e 0a             | go..<..X.3N.
+        *
+        * */
+        FileOutputStream(rdbFile).use { fos ->
+            fos.write(byteArrayOf(
+                0x52, 0x45, 0x44, 0x49, 0x53, 0x30, 0x30, 0x31, 0x31, 0xfa.toByte(), 0x09, 0x72, 0x65, 0x64, 0x69, 0x73,
+                0x2d, 0x76, 0x65, 0x72, 0x05, 0x37, 0x2e, 0x32, 0x2e, 0x30, 0xfa.toByte(), 0x0a, 0x72, 0x65, 0x64, 0x69,
+                0x73, 0x2d, 0x62, 0x69, 0x74, 0x73, 0xc0.toByte(), 0x40, 0xfe.toByte(), 0x00, 0xfb.toByte(), 0x05, 0x00, 0x00, 0x09, 0x72,
+                0x61, 0x73, 0x70, 0x62, 0x65, 0x72, 0x72, 0x79, 0x0a, 0x73, 0x74, 0x72, 0x61, 0x77, 0x62, 0x65, 0x72,
+                0x72, 0x79, 0x00, 0x06, 0x62, 0x61, 0x6e, 0x61, 0x6e, 0x61, 0x05, 0x61, 0x70, 0x70, 0x6c, 0x65, 0x00,
+                0x0a, 0x73, 0x74, 0x72, 0x61, 0x77, 0x62, 0x65, 0x72, 0x72, 0x79, 0x06, 0x6f, 0x72, 0x61, 0x6e, 0x67,
+                0x65, 0x00, 0x05, 0x61, 0x70, 0x70, 0x6c, 0x65, 0x09, 0x70, 0x69, 0x6e, 0x65, 0x61, 0x70, 0x70, 0x6c,
+                0x65, 0x00, 0x05, 0x67, 0x72, 0x61, 0x70, 0x65, 0x05, 0x6d, 0x61, 0x6e, 0x67, 0x6f, 0xff.toByte(), 0x04,
+                0x3c, 0x14, 0x16, 0x58, 0x10, 0x33, 0x4e, 0x0a
+            ))
+        }
+
+        // Step 2: Run the Redis server
+        val serverThread = Thread {
+            main(arrayOf("--dir", "/tmp/rdbfiles3974263201", "--dbfilename", "orange.rdb"))
+        }
+        serverThread.start()
+
+        // Wait for the server to start
+        Thread.sleep(1000)
+
+        // Step 3: Execute the GET command
+        val socket = Socket("localhost", 6379)
+        val writer = OutputStreamWriter(socket.getOutputStream())
+        val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+
+        writer.write("*2\r\n\$3\r\nGET\r\n\$9\r\nraspberry\r\n")
+        writer.flush()
+
+        writer.write("*2\r\n\$3\r\nGET\r\n\$5\r\ngrape\r\n")
+        writer.flush()
+
+        val response = reader.readLine()
+        assertEquals("\$10", response) // Length of "strawberry"
+        assertEquals("strawberry", reader.readLine())
+
+        val response2 = reader.readLine()
+        assertEquals("\$5", response2) // Length of "banana"
+        assertEquals("mango", reader.readLine())
+
+        socket.close()
+        serverThread.interrupt()
+    }
 }
